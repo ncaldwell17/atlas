a5a9bf6c2704b52c5d017291d4c979e63211e7c9
ncaldwell17 <ncaldwellgatsos@gmail.com>
2020-09-28 22:45:47 -0500

    prepping for initial frontpage dev

---
+++ atlas_config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+"""
+Project-wide application configuration.
+DO NOT STORE SECRETS, PASSWORDS, ETC. IN THIS FILE.
+They will be exposed to users. Use environment variables instead.
+See get_secrets() below for a fast way to access them.
+"""
+
+# NAMES
+
+
+# DEPLOYMENT
+
+
+# SHARING
+
+
+# SERVICES
+
+
+# OAUTH PATHS
+
+
+# LOGGING
+
+
+# CONFIGURATION UTILITIES
+
